0b82bdc921a457ee8a321e14904ee90029bd0ac6
swdbyouthful <whgurrbs@gmail.com>
2025-04-27 11:41:50 +0900

    fix: edit favicon setting in layout.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="icon" href="/swdb.ico" />
-      </Head>
       <body>
         <Main />
         <NextScript />
