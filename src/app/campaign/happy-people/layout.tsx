445f4b58e1053aea4ac3ed488b58cae318115f1f
swdbyouthful <whgurrbs@gmail.com>
2025-04-27 12:00:11 +0900

    feat: add layout for happy-people

---
+++ src/app/campaign/happy-people/layout.tsx
@@ -0,0 +1,11 @@
+// layout.tsx
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '수원동부교회 2025 행복한 사람들의 축제',
+  description: '수원동부교회 2025 행복한 사람들의 축제',
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
