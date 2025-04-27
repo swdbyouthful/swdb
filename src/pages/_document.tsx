0dd02c0a908c9b04156b9a6fed560e5fc1fc2b6c
swdbyouthful <whgurrbs@gmail.com>
2025-04-27 11:38:11 +0900

    fix: edit favicon path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="%PUBLIC_URL%/swdb.ico" />
+        <link rel="icon" href="/swdb.ico" />
       </Head>
       <body>
         <Main />
