bd3d8851ed6921ab243137ba1072d044d5043d73
swdbyouthful <whgurrbs@gmail.com>
2025-04-27 10:08:56 +0900

    ci: add assetPrefix to nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/https://swdbyouthful.github.io/swdb' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://swdbyouthful.github.io/swdb' : '',
 };
 
 export default nextConfig;
