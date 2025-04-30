420f8d9a8c797606be41b2f42edb2bd94efde996
swdbyouthful <whgurrbs@gmail.com>
2025-04-30 11:31:38 +0900

    build: NextConfig.output 활성화

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
+  output: 'export',
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://swdbyouthful.github.io/swdb' : '',
   // images: {
   //   loader: 'imgix',
