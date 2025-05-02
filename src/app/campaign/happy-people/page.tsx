3a5c0c19cec2d2ee8c4537f3470f6c7d70502756
swdbyouthful <whgurrbs@gmail.com>
2025-05-02 14:31:14 +0900

    fix: [style] 초대장 본문 행간 1.3

--- src/app/campaign/happy-people/page.tsx
+++ src/app/campaign/happy-people/page.tsx
@@ -50,7 +50,7 @@ const CampaignPage = () => {
           </div>
 
           {/* TODO: 초대하는 글 확인후 변경 */}
-          <div className={'chosunilbo_myungo-regular text-[12px]'}>
+          <div className={'chosunilbo_myungo-regular text-[12px] leading-[1.3]'}>
             <p className={'mt-[8px]'}>행복한 사람들의 축제는 교회 공동체 속에</p>
             <p className={''}>전도운동과 영적 각성운동을 동시에 불러일으키는</p>
             <p className={''}>전 교회적 운동으로 세상에 복음을 편만하게 선포하는 것이</p>
