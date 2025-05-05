8c01ca5e48de4699a4afa711278374eb7171354e
swdbyouthful <whgurrbs@gmail.com>
2025-05-06 08:03:49 +0900

    fix: layout seo meta tag

--- src/app/campaign/happy-people/layout.tsx
+++ src/app/campaign/happy-people/layout.tsx
@@ -3,7 +3,21 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: '행복한 사람들의 봄 축제',
-  description: '수원동부교회 2025 행복한 사람들의 축제',
+  description: '수원동부교회 2025 행복한 사람들의 축제에 여러분을 초대합니다.',
+  openGraph: {
+    title: '행복한 사람들의 봄 축제',
+    description: '수원동부교회 2025 행복한 사람들의 축제에 여러분을 초대합니다.',
+    url: 'https://swdbyouthful.github.io/swdb/campaign/happy-people',
+    type: 'website',
+    images: [
+      {
+        url: 'https://swdbyouthful.github.io/swdb/image/main_banner.png',
+        width: 1280,
+        height: 720,
+        alt: '행복한 사람들의 봄 축제 초대장',
+      },
+    ],
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
